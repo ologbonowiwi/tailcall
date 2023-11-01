2b42114a3b69c53c0da0b0f03bdcf13f64baa2e5
Sujeet Sreenivasan <sujeetsr@gmail.com>
2023-11-01 10:05:01 -0700

    test all requests with batch requests enabled

--- tests/batched_request_spec.rs
+++ tests/batched_request_spec.rs
@@ -2,6 +2,14 @@ mod integration_tests {
   use serde::{Deserialize, Serialize};
   use serde_json::json;
 
+  async fn make_request(http_client: reqwest::Client, body: String) -> reqwest::Response {
+    http_client
+      .post("http://localhost:8000/graphql")
+      .header("Content-Type", "application/json")
+      .body(body)
+      .send().await.expect("Failed to send request")
+  }
+
   async fn initiate_test_server(mock_schema_path: String) -> &'static str {
     let config = tailcall::config::Config::from_file_paths([mock_schema_path].iter())
       .await
@@ -28,7 +36,7 @@ mod integration_tests {
   }
 
   #[tokio::test]
-  async fn test_batched_request() {
+  async fn test_requests_with_batch_requests_enabled() {
     let schema_path = "tests/graphql_mock/test-batched-request.graphql";
 
     tokio::spawn(initiate_test_server(schema_path.into()));
@@ -36,23 +44,23 @@ mod integration_tests {
 
     let http_client = reqwest::Client::new();
 
-    let query_1_data = json!({"query": "query {post(id: 1) {title}}"});
-    let query_2_data = json!({"query": "query { post(id: 2) { title } }"});
-    let batched_query_data = format!("[{},{}]", query_1_data, query_2_data);
-
-    let api_request = http_client
-      .post("http://localhost:8000/graphql")
-      .header("Content-Type", "application/json")
-      .body(batched_query_data);
-
-    let response = api_request.send().await.expect("Failed to send request");
+    let query1 = json!({"query": "query { post(id: 1) { title } }"});
+    let query2 = json!({"query": "query { post(id: 2) { title } }"});
+    
+    let batched_query = format!("[{},{}]", query1, query2);
+    let response = make_request(http_client.clone(), batched_query).await;
     let json = response.json::<Vec<Resp>>().await.unwrap();
-
     assert_eq!(json.len(), 2);
     assert_eq!(
       json.get(0).unwrap().data.post.title,
       "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
     );
     assert_eq!(json.get(1).unwrap().data.post.title, "qui est esse");
+
+    let unbatched_query = format!("{}", query1);
+    let response = make_request(http_client, unbatched_query).await;
+    let json = response.json::<Resp>().await.unwrap();
+    assert_eq!(json.data.post.title, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
   }
 }
+
